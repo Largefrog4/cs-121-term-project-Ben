75e23444682fbb4fb13f48062369242e7790b713
Ben <bif@metafreed.net>
2023-12-09 13:31:24 -0500

    finished

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import random
 
-deck = [11,11,11,11,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+deck = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
 
     
 
-    
-def ddraw():
-    i=deck[0]
-    del deck[0]
-    print(f'The dealer drew a {i}')
-    return i
-    
 class Player:
     def __init__(self):
         self.hand=[]
@@ -44,42 +37,41 @@ def main():
     dealer.draw()
     print('Your hand: ',player.gethand())
     print("Dealer's hand ",dealer.gethand())
-    if player.gethand() == 21 and dealer.gethand() == 21:
-        print('draw')
-    elif player.gethand()==21:
-        print('Blackjack, you win.')
+    inp=input('Hit or stand? ')
+    while inp !='hit' and inp != 'Hit' and inp != 'stand' and inp != 'Stand':
+            print("invalid input")
+            inp=input('Hit or stand? ')
+    while inp == 'hit' or inp =='Hit':
+        player.draw()
+        print('Your hand: ',player.gethand())
+        print("Dealer's hand ",dealer.gethand())
+        if player.gethand()==21:
+            print('Blackjack, you win.')
+            exit()
+        elif player.gethand() > 21:
+            print('Bust, dealer wins')
+            exit()
+        inp=input('Hit or stand? ')
+        while inp !='hit' and inp != 'Hit' and inp != 'stand' and inp != 'Stand':
+            print("invalid input")
+            inp=input('Hit or stand? ')
+        if inp == 'stand' or inp=='Stand':
+            break
+        
+    while dealer.gethand() < 17:
+        dealer.draw()
+    print('Your hand: ',player.gethand())
+    print("Dealer's hand ",dealer.gethand())
+    if dealer.gethand() >21:
+        print('Bust, you win')
     elif dealer.gethand()==21:
         print('Blackjack, dealer wins.')
-    elif player.gethand() > 21:
-        print('Bust, dealer wins')
-    elif dealer.gethand() >21:
-        print('Bust, you win')
-    else:
-        inp=input('Hit or stand? ')
-        while inp == 'hit' or 'Hit':
-            player.draw()
-            print('Your hand: ',player.gethand())
-            print("Dealer's hand ",dealer.gethand())
-            if player.gethand()==21:
-                print('Blackjack, you win.')
-                break
-            elif player.gethand() > 21:
-                print('Bust, dealer wins')
-                break
-            inp=input('Hit or stand? ')
-        if inp == 'Stand' or 'stand':
-            while dealer.gethand() < 17:
-                dealer.draw()
-        if dealer.gethand() >21:
-            print('Bust, you win')
-        elif dealer.gethand()==21:
-            print('Blackjack, dealer wins.')
-        elif dealer.gethand() > player.gethand():
-            print('Dealer wins')
-        elif dealer.gethand() < player.gethand():
-            print('you win')
-        elif dealer.gethand() == player.gethand():
-            print('draw')
+    elif dealer.gethand() > player.gethand():
+        print('Dealer wins')
+    elif dealer.gethand() < player.gethand():
+        print('you win')
+    elif dealer.gethand() == player.gethand():
+        print('draw')
     
     return
 
